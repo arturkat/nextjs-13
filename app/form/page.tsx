b08fb6164e241585aeec3eb1ca9efc96c0631246
Arthur Katrushko <arthur.katrushko@fenomdigital.com>
2023-02-23 21:50:24 +0200

    Context menu for the content explorer

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -4,7 +4,7 @@ import { NavbarOffset } from "@comp/Navbar";
 import TextInput from "@ui/TextInput";
 import Textarea from "@ui/Textarea";
 import Button from "@ui/Button";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { host } from "@/lib/config";
 
 export default function Form() {
@@ -70,21 +70,22 @@ export default function Form() {
         <h1>Forms</h1>
 
         <h2>Image Library</h2>
-        <Button onClick={() => getFolderContent()} className="mb-5">
-          Get Images
-        </Button>
-        <nav>
-          <Button onClick={() => getFolderContent("", true)} className="mb-1">
-            Back
-          </Button>
+        <ContextMenu />
+        <nav className="mb-4">
+          <Button onClick={() => getFolderContent()}>Get Images</Button>
+          <Button onClick={() => getFolderContent("", true)}>Back</Button>
         </nav>
-        <div className="grid grid-flow-row-dense grid-cols-6 gap-4 border-2 p-5">
+        <div
+          className="grid grid-flow-row-dense grid-cols-6 gap-4 border-2 rounded-md p-5"
+          data-context-menu="canvas"
+        >
           {folderContent.map((item) => {
             if (item.isFolder) {
               return (
                 <div
                   onClick={() => getFolderContent(item.name)}
-                  className="border-amber-400 border-2 rounded-md p-2 radius break-words cursor-pointer"
+                  data-context-menu="folder"
+                  className="border-amber-500 border-2 rounded-md bg-amber-200 p-2 radius break-words cursor-pointer"
                   key={item.name}
                 >
                   Folder: {item.name}
@@ -92,8 +93,12 @@ export default function Form() {
               );
             } else {
               return (
-                <figure className="grid grid-cols-1 m-0" key={item.name}>
-                  <img src={item.url} className="" />
+                <figure
+                  data-context-menu="image"
+                  className="grid grid-cols-1 m-0 p-2 rounded-md border-2 border-amber-500 bg-amber-50"
+                  key={item.name}
+                >
+                  <img src={item.url} className="rounded-md" />
                   <figcaption className="self-end break-words">
                     {item.name}
                   </figcaption>
@@ -106,7 +111,7 @@ export default function Form() {
         <h2>Upload Image</h2>
         <form
           onSubmit={uploadToServer}
-          action="http://localhost:3000/api/file-upload"
+          action={`${host}/api/file-upload`}
           className={`mb-10`}
         >
           <input type="file" name="file" onChange={uploadToClient} />
@@ -128,4 +133,75 @@ export default function Form() {
       </div>
     </>
   );
+}
+
+function ContextMenu(props) {
+  let { cords, setCords, visible, setVisible, target } = useContextMenu();
+
+  return (
+    visible && (
+      <div
+        className="absolute inline-grid gap-2 min-w-[7rem] bg-indigo-300 shadow-lg shadow-indigo-400 rounded-md px-5 py-3 my-2"
+        style={{ top: cords.y, left: cords.x }}
+      >
+        {target.dataset.contextMenu === "folder" && (
+          <>
+            <a
+              className="text-xl cursor-pointer"
+              onClick={() => target.click()}
+            >
+              Open
+            </a>
+            <a className="text-xl cursor-pointer">Delete</a>
+            <a className="text-xl cursor-pointer">Exit</a>
+          </>
+        )}
+        {target.dataset.contextMenu === "image" && (
+          <>
+            <a className="text-xl cursor-pointer">Delete</a>
+            <a className="text-xl cursor-pointer">Exit</a>
+          </>
+        )}
+        {target.dataset.contextMenu === "canvas" && (
+          <>
+            <a className="text-xl cursor-pointer">Create Folder</a>
+            <a className="text-xl cursor-pointer">Exit</a>
+          </>
+        )}
+      </div>
+    )
+  );
+}
+
+function useContextMenu() {
+  let [cords, setCords] = useState({ x: 0, y: 0 });
+  let [visible, setVisible] = useState(false);
+  let [target, setTarget] = useState(null);
+
+  function handleContextMenu(e) {
+    const iTarget = e.target.closest("[data-context-menu]");
+    if (iTarget) {
+      e.preventDefault();
+      setTarget(iTarget);
+      setCords({ x: e.pageX, y: e.pageY });
+      setVisible(true);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("click", () => setVisible(false));
+    return () => {
+      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("click", () => setVisible(false));
+    };
+  }, []);
+
+  return {
+    cords,
+    setCords,
+    visible,
+    setVisible,
+    target,
+  };
 }
